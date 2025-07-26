15dfbd070f751fddbb40109e6f7e763b7fa8beda
Sarvesh <sarveshshripad.kulkarni@cognizant.com>
2025-07-26 16:48:10 +0530

    feature updated

--- RideShareConnect/Model/VehicleModel/VehicleDocumentModel.cs
+++ RideShareConnect/Model/VehicleModel/VehicleDocumentModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RideShareConnect.Models
 {
-    public class VehicleDocument
+    public class VehicleDocumentModel
     {
         [Key]
         public int DocumentId { get; set; }
