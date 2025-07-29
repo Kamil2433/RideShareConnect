90acb7cb6782bce7007434e288a5dec6aa654dcc
Sarvesh <sarveshshripad.kulkarni@cognizant.com>
2025-07-29 10:31:30 +0530

    migrations updated

--- RideShareConnect/Migrations/20250725044634_InitialCreate.cs
+++ RideShareConnect/Migrations/20250725044634_InitialCreate.cs
@@ -11,7 +11,6 @@ namespace RideShareConnect.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            // Users
             migrationBuilder.CreateTable(
                 name: "Users",
                 columns: table => new
@@ -30,27 +29,6 @@ namespace RideShareConnect.Migrations
                     table.PrimaryKey("PK_Users", x => x.UserId);
                 });
 
-            // DriverProfiles
-            migrationBuilder.CreateTable(
-                name: "DriverProfiles",
-                columns: table => new
-                {
-                    DriverProfileId = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    LicenseNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    LicenseExpiry = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    ExperienceYears = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_DriverProfiles", x => x.DriverProfileId);
-                });
-
-            // TwoFactorCodes
             migrationBuilder.CreateTable(
                 name: "TwoFactorCodes",
                 columns: table => new
@@ -74,7 +52,6 @@ namespace RideShareConnect.Migrations
                         onDelete: ReferentialAction.Cascade);
                 });
 
-            // UserProfiles
             migrationBuilder.CreateTable(
                 name: "UserProfiles",
                 columns: table => new
@@ -99,7 +76,6 @@ namespace RideShareConnect.Migrations
                         onDelete: ReferentialAction.Cascade);
                 });
 
-            // UserSettings
             migrationBuilder.CreateTable(
                 name: "UserSettings",
                 columns: table => new
@@ -126,119 +102,44 @@ namespace RideShareConnect.Migrations
                         onDelete: ReferentialAction.Cascade);
                 });
 
-            // Vehicles
-            migrationBuilder.CreateTable(
-                name: "Vehicles",
-                columns: table => new
-                {
-                    VehicleId = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    DriverProfileId = table.Column<int>(type: "int", nullable: false),
-                    Make = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Model = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Year = table.Column<int>(type: "int", nullable: false),
-                    LicensePlate = table.Column<string>(type: "nvarchar(max)", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Vehicles", x => x.VehicleId);
-                    table.ForeignKey(
-                        name: "FK_Vehicles_DriverProfiles_DriverProfileId",
-                        column: x => x.DriverProfileId,
-                        principalTable: "DriverProfiles",
-                        principalColumn: "DriverProfileId",
-                        onDelete: ReferentialAction.Cascade);
-                });
+            migrationBuilder.CreateIndex(
+                name: "IX_TwoFactorCodes_UserId",
+                table: "TwoFactorCodes",
+                column: "UserId");
 
-            // DriverRatings
-            migrationBuilder.CreateTable(
-                name: "DriverRatings",
-                columns: table => new
-                {
-                    DriverRatingId = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    DriverProfileId = table.Column<int>(type: "int", nullable: false),
-                    RatingValue = table.Column<int>(type: "int", nullable: false),
-                    Review = table.Column<string>(type: "nvarchar(max)", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_DriverRatings", x => x.DriverRatingId);
-                    table.ForeignKey(
-                        name: "FK_DriverRatings_DriverProfiles_DriverProfileId",
-                        column: x => x.DriverProfileId,
-                        principalTable: "DriverProfiles",
-                        principalColumn: "DriverProfileId",
-                        onDelete: ReferentialAction.Cascade);
-                });
+            migrationBuilder.CreateIndex(
+                name: "IX_UserProfiles_UserId",
+                table: "UserProfiles",
+                column: "UserId",
+                unique: true);
 
-            // MaintenanceRecords
-            migrationBuilder.CreateTable(
-                name: "MaintenanceRecords",
-                columns: table => new
-                {
-                    MaintenanceRecordId = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    VehicleId = table.Column<int>(type: "int", nullable: false),
-                    MaintenanceDate = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_MaintenanceRecords", x => x.MaintenanceRecordId);
-                    table.ForeignKey(
-                        name: "FK_MaintenanceRecords_Vehicles_VehicleId",
-                        column: x => x.VehicleId,
-                        principalTable: "Vehicles",
-                        principalColumn: "VehicleId",
-                        onDelete: ReferentialAction.Cascade);
-                });
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
 
-            // VehicleDocuments
-            migrationBuilder.CreateTable(
-                name: "VehicleDocuments",
-                columns: table => new
-                {
-                    VehicleDocumentId = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    VehicleId = table.Column<int>(type: "int", nullable: false),
-                    DocumentType = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    ExpiryDate = table.Column<DateTime>(type: "datetime2", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_VehicleDocuments", x => x.VehicleDocumentId);
-                    table.ForeignKey(
-                        name: "FK_VehicleDocuments_Vehicles_VehicleId",
-                        column: x => x.VehicleId,
-                        principalTable: "Vehicles",
-                        principalColumn: "VehicleId",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
-            // Indexes
-            migrationBuilder.CreateIndex(name: "IX_TwoFactorCodes_UserId", table: "TwoFactorCodes", column: "UserId");
-            migrationBuilder.CreateIndex(name: "IX_UserProfiles_UserId", table: "UserProfiles", column: "UserId", unique: true);
-            migrationBuilder.CreateIndex(name: "IX_Users_Email", table: "Users", column: "Email", unique: true);
-            migrationBuilder.CreateIndex(name: "IX_UserSettings_UserId", table: "UserSettings", column: "UserId", unique: true);
-            migrationBuilder.CreateIndex(name: "IX_Vehicles_DriverProfileId", table: "Vehicles", column: "DriverProfileId");
-            migrationBuilder.CreateIndex(name: "IX_DriverRatings_DriverProfileId", table: "DriverRatings", column: "DriverProfileId");
-            migrationBuilder.CreateIndex(name: "IX_MaintenanceRecords_VehicleId", table: "MaintenanceRecords", column: "VehicleId");
-            migrationBuilder.CreateIndex(name: "IX_VehicleDocuments_VehicleId", table: "VehicleDocuments", column: "VehicleId");
+            migrationBuilder.CreateIndex(
+                name: "IX_UserSettings_UserId",
+                table: "UserSettings",
+                column: "UserId",
+                unique: true);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(name: "DriverRatings");
-            migrationBuilder.DropTable(name: "MaintenanceRecords");
-            migrationBuilder.DropTable(name: "VehicleDocuments");
-            migrationBuilder.DropTable(name: "TwoFactorCodes");
-            migrationBuilder.DropTable(name: "UserProfiles");
-            migrationBuilder.DropTable(name: "UserSettings");
-            migrationBuilder.DropTable(name: "Vehicles");
-            migrationBuilder.DropTable(name: "Users");
-            migrationBuilder.DropTable(name: "DriverProfiles");
+            migrationBuilder.DropTable(
+                name: "TwoFactorCodes");
+
+            migrationBuilder.DropTable(
+                name: "UserProfiles");
+
+            migrationBuilder.DropTable(
+                name: "UserSettings");
+
+            migrationBuilder.DropTable(
+                name: "Users");
         }
     }
 }
